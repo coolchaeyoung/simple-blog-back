68dfb9b1c2bf4071f446d8ddc6c68dcc900ead70
박채영 <ggodchae@naver.com>
2021-02-19 21:54:12 +0900

    User Schema

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,12 +1,12 @@
 import Router from "koa-router";
-import { write, list, read, remove, update } from "./posts.ctrl";
+import { write, list, read, remove, update, checkObjectId } from "./posts.ctrl";
 
 const posts = new Router();
 
 posts.get("/", list);
 posts.post("/", write);
-posts.get("/:id", read);
-posts.delete("/:id", remove);
-posts.patch("/:id", update);
+posts.get("/:id", checkObjectId, read);
+posts.delete("/:id", checkObjectId, remove);
+posts.patch("/:id", checkObjectId, update);
 
 export default posts;
